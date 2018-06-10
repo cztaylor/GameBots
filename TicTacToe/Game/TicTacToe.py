f296fcff31af4454ac8d1ca4c681fd093ac066a6
Christian Taylor <chrtaylo@microsoft.com>
2018-06-09 17:19:36 -0700

    added turn function

--- TicTacToe/Game/TicTacToe.py
+++ TicTacToe/Game/TicTacToe.py
@@ -7,22 +7,29 @@ Created on Sun May  6 13:47:41 2018
 
 import board;
 
-
-
 b = board.Board()
 
-
-
-
-
 b.move("X",1)
-b.move("X",5)
-b.move("X",9)
-b.move("O",6)
-b.move("O",3)
 
-b.printBoardStateCoordinates()
+def turn(board,team):
+    space = int(input("Select a space for team " + team + ": "))
+    result = b.move(team,space)
+    if result == -1:
+        print("Space already taken. Please select another spot.")
+        turn(board,team)
+    print(result)
+    b.printBoardState()
 
-status = b.checkStatus("X")
+turn(b,"X")
 
-print(status)
+#b.move("X",1)
+#b.move("X",5)
+#b.move("X",9)
+#b.move("O",6)
+#b.move("O",3)
+#
+#b.printBoardStateCoordinates()
+#
+#status = b.checkStatus("X")
+#
+#print(status)
