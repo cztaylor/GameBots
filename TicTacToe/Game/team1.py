b602fa5d548eaa4493827c91c12c48f2d83bff32
Christian Taylor <chrtaylo@microsoft.com>
2018-10-06 12:13:03 -0700

    made bot smarter

--- TicTacToe/Game/team1.py
+++ TicTacToe/Game/team1.py
@@ -2,9 +2,17 @@
 
 
 class Team1:
-    def __init__(self,team):
-    	self.team = team
+	def __init__(self,team):
+		self.team = team
 
-    def makeMove(self,boardState):
-    	return(1)
+	def makeMove(self,boardState):
+		for i in range(9):
+			row = (i - 1) // 3
+			col = (i - 1) % 3
+			if(boardState[row][col] == " "):
+				return(i)
+
+
+
+    	
 
